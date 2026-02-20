e760fc2d03410527f2529a51dc3156c20db816d5
colours93 <student@cognitiveclass.ai>
2026-02-20 11:02:54 -0500

    Fix pylint: add docstrings, fix line length

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Flask server for emotion detection application."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +6,7 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """Detect emotions in the given text."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
@@ -15,10 +17,15 @@ def sent_analyzer():
     joy = response['joy']
     sadness = response['sadness']
     dominant = response['dominant_emotion']
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant}."
+    output = "For the given statement, the system response is "
+    output += f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+    output += f"'joy': {joy} and 'sadness': {sadness}. "
+    output += f"The dominant emotion is {dominant}."
+    return output
 
 @app.route("/")
 def render_index_page():
+    """Render the index page."""
     return render_template('index.html')
 
 if __name__ == "__main__":
